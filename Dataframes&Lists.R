08deb1e60c5a3b601663a720500a1b387e02d754
TheShwarna <7s7hjv5jn8@privaterelay.appleid.com>
2026-02-08 16:30:22 +0530

    Update Dataframes&Lists.R

--- Dataframes&Lists.R
+++ Dataframes&Lists.R
@@ -106,5 +106,77 @@ sports_2 = cbind(sports_2,federation)
 mean_minutes <- mean(sports_2[,"minutes"])
 
 #Lists
+multi_object <- list (
+  "Vector"= c(1,2,3),
+  "Array"= array(1:3, dim=c(2,2)),
+  "Logical"= TRUE
+)
+
+names(multi_object) <- c('VECTRO','ARAY','Logic')
+
+#To call a data point in the 
+multi_object[1]#to select list level
+multi_object[[1]][2]#to select underlying list level
+multi_object[[2]][2,2]
+multi_object[c(1,2)] #to call list level values
+multi_object['Vector']
+multi_object$Vector #$ sign works ony for named indexes
+multi_object$Vector[2]
+
+example_list <- list(c('a','b','c'),array(1:10, dim= c(2,5)))
+#Add item in the list
+example_list[[3]]<- c(1,2,3)
+#Remove item, [[]] and [] works same
+example_list[3] <- NULL
+
+#Exercises
+
+# Create a list with 3 elements: 
+# - a character with the element "John"
+# - a vector with the values 100, 250, 300, 25
+# - a matrix with the values (1,1,0,0) with 
+# two rows and two cols
+
+# store the returning object in an object 
+# called customer
+
+customer = list(c('John'),c(100, 250, 300, 25),matrix(c(1,1,0,0),nrow=2, ncol=2))
+
+# Add a fourth element to our list customer 
+# the value 2015
+
+customer[4] <- c(2015)
+
+# Add names to our list elements 
+# The names of our elements should be: 
+# "Name", "Balance", "Products", "Opening Date"
+
+names(customer) <- c("Name", "Balance", "Products", "Opening Date")
+
+# Change the customer last Balance value to 10
+# Change the customer object itself
+
+customer[[2]][4] <- 10
+
+# Store the products matrix in an object called
+# products 
+
+products <- customer$Products
+
+# Create a new customer (a list) with two 
+# elements - a character with "Anne"
+# and a vector with c(400, 232, 294, 593)
+
+# name the object new_customer and give names
+# to the list elements with name "Name_2" and "Balance_2"
+
+new_customer = list(c("Anne"),c(400, 232, 294, 593))
+
+# Join the two lists in a new list called 
+# final_list 
+
+final_list = c(customer,new_customer)
+
+
 
 
